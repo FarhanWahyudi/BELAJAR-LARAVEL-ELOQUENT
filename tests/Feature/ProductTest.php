fa954c458333ebc8227cde20643da0988cd5b4bb
farhan wahyudi <farhanwahyudi875@gmail.com>
2025-05-31 16:33:31 +0700

    serialization

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -36,4 +36,15 @@ class ProductTest extends TestCase
 
         $this->assertEquals(1, $product[0]->id);
     }
+    
+    public function testSerialization()
+    {
+        $this->seed([CategorySeeder::class, ProductSeeder::class]);
+
+        $products = Product::get()->load('category');
+        $this->assertCount(2, $products);
+
+        $json = $products->toJson(JSON_PRETTY_PRINT);
+        Log::info($json);
+    }
 }
